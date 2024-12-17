54d2b7ab421e3db8efffef8e58ebc6e49fe1a632
Olli Könönen <olli.kononen@nokia.com>
2024-12-17 20:46:42 +0200

    model start

---
+++ bookRec.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.neighbors import NearestNeighbors
+
+
+df = pd.read(r"BooksDataset.csv")
+
+
+
+
+
